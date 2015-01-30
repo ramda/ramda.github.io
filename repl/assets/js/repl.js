3c1cd500855f911b365b83b92f7661cd688554ec
buzzdecafe <m_hur@yahoo.com>
2015-01-30 18:57:22 -0500

    dynamic repl

---
+++ repl/assets/js/repl.js
@@ -0,0 +1,356 @@
+// modified by @AutoSponge 2013.
+//
+// Copyright 2011 Traceur Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+(function(global) {
+    'use strict';
+
+    // Do not show source maps by default.
+    traceur.options.sourceMaps = false;
+
+    var SourceMapConsumer = traceur.outputgeneration.SourceMapConsumer;
+    var SourceMapGenerator = traceur.outputgeneration.SourceMapGenerator;
+    var ProjectWriter = traceur.outputgeneration.ProjectWriter;
+    var ErrorReporter = traceur.util.ErrorReporter;
+
+    var hasError = false;
+    var debouncedCompile = debounced(compile, 200, 2000);
+    var input = CodeMirror.fromTextArea(document.querySelector('.input'), {
+        onChange: debouncedCompile,
+        onCursorActivity: debouncedCompile.delay,
+        lineNumbers: true,
+        theme: "ambiance"
+    });
+
+    var evalElement = document.querySelector('pre.eval');
+    var errorElement = document.querySelector('pre.error');
+    var sourceMapElement = document.querySelector('pre.source-map');
+
+    if (location.hash)
+        input.setValue(decodeURIComponent(location.hash.slice(1)));
+
+    /**
+     * debounce time = min(tmin + [func's execution time], tmax).
+     *
+     * @param {Function} func
+     * @param {number} tmin Minimum debounce time
+     * @param {number} tmax Maximum debounce time
+     * @return {Function} A debounced version of func with an attached "delay"
+     *     function. "delay" will delay any pending debounced function by the
+     *     current debounce time. If there are none pending, it is a no-op.
+     */
+    function debounced(func, tmin, tmax) {
+        var id = 0;
+        var t = tmin;
+        function wrappedFunc() {
+            var start = Date.now();
+            id = 0;
+            func();
+            t = tmin + Date.now() - start; // tmin + [func's execution time]
+            t = t < tmax ? t : tmax;
+        }
+        function debouncedFunc() {
+            clearTimeout(id);
+            id = setTimeout(wrappedFunc, t);
+        }
+        // id is nonzero only when a debounced function is pending.
+        debouncedFunc.delay = function() { id && debouncedFunc(); }
+        return debouncedFunc;
+    }
+
+    function setOptionsFromSource(source) {
+        var re = /^\/\/ Options:\s*(.+)$/mg;
+        var optionLines = source.match(re);
+        if (optionLines) {
+            optionLines.forEach(function(line) {
+                re.lastIndex = 0;
+                var m = re.exec(line);
+                try {
+                    traceur.options.fromString(m[1]);
+                } catch (ex) {
+                    // Ignore unknown options.
+                }
+            });
+            createOptions();
+        }
+    }
+
+    function compile() {
+        hasError = false;
+        errorElement.textContent = sourceMapElement.textContent = '';
+
+        var reporter = new ErrorReporter();
+        reporter.reportMessageInternal = function(location, format, args) {
+            errorElement.textContent +=
+                ErrorReporter.format(location, format, args) + '\n';
+        };
+
+        var url = location.href;
+        var project = new traceur.semantics.symbols.Project(url);
+        var name = 'repl';
+        var contents = input.getValue();
+        if (history.replaceState)
+            history.replaceState(null, document.title,
+                '#' + encodeURIComponent(contents));
+        setOptionsFromSource(contents);
+        var sourceFile = new traceur.syntax.SourceFile(name, contents);
+        project.addFile(sourceFile);
+        var res = traceur.codegeneration.Compiler.compile(reporter, project, false);
+        if (reporter.hadError()) {
+            hasError = true;
+        } else {
+            var options;
+            if (traceur.options.sourceMaps) {
+                var config = {file: 'traceured.js'};
+                var sourceMapGenerator = new SourceMapGenerator(config);
+                options = {sourceMapGenerator: sourceMapGenerator};
+            }
+
+            var source = ProjectWriter.write(res, options);
+
+            try {
+                evalElement.textContent = ('global', eval)(source);
+            } catch(ex) {
+                hasError = true;
+                errorElement.textContent = ex;
+            }
+
+            if (traceur.options.sourceMaps) {
+                var renderedMap = renderSourceMap(source, options.sourceMap);
+                sourceMapElement.textContent = renderedMap;
+            }
+        }
+
+        errorElement.hidden = !hasError;
+    }
+
+    function createOptionRow(name) {
+        var li = document.createElement('li');
+        var label = document.createElement('label');
+        label.textContent = name;
+        var cb = label.insertBefore(document.createElement('input'),
+            label.firstChild);
+        cb.type = 'checkbox';
+        var checked = traceur.options[name];
+        cb.checked = checked;
+        cb.indeterminate = checked === null;
+        cb.onclick = function() {
+            traceur.options[name] = cb.checked;
+            createOptions();
+            compile();
+        };
+        li.appendChild(label);
+        return li;
+    }
+
+    var options = [
+        'experimental',
+        'debug',
+        'sourceMaps',
+        'freeVariableChecker',
+        'validate'
+    ];
+
+    var showAllOpts = true;
+    var allOptsLength = Object.keys(traceur.options).length;
+    var showMax = allOptsLength;
+
+    function createOptions() {
+        var optionsDiv = document.querySelector('.traceur-options');
+        optionsDiv.textContent = '';
+        if (showAllOpts) {
+            var i = 0;
+            Object.keys(traceur.options).forEach(function(name) {
+                if (i++ >= showMax || options.lastIndexOf(name) >= 0)
+                    return;
+                optionsDiv.appendChild(createOptionRow(name));
+            });
+            optionsDiv.appendChild(document.createElement('hr'));
+        }
+        options.forEach(function(name) {
+            optionsDiv.appendChild(createOptionRow(name));
+        });
+    }
+
+    createOptions();
+
+    function renderSourceMap(source, sourceMap) {
+        var consumer = new SourceMapConsumer(sourceMap);
+        var lines = source.split('\n');
+        var lineNumberTable = lines.map(function(line, lineNo) {
+            var generatedPosition = {
+                line: lineNo + 1,
+                column: 0
+            };
+            var position = consumer.originalPositionFor(generatedPosition);
+            var lineDotColumn = position.line + '.' + position.column;
+            return (lineNo + 1) + ': ' + line + ' -> ' + lineDotColumn;
+        });
+        return 'SourceMap:\n' + lineNumberTable.join('\n');
+    }
+
+    global.cm = input;
+
+}(this));
+
+(function () {
+
+    var examples = document.getElementById("examples");
+    var elms = {};
+    var cache = {};
+    var config = {
+        arrays: [
+            {
+                title: "map",
+                body: function () {
+                    /*
+R.map(function(x) { return x + 1; }, [10, 20, 30]);
+                     */
+                }
+            },
+            {
+                title: 'filter',
+                body: function() {
+                    /*                  
+R.filter(function(x) { return x > 10; }, [10, 20, 3, 100, 33, 1]);
+                     */
+                }
+            },
+            {
+                title: 'foldl',
+                body: function() {
+                    /*
+R.foldl(function(acc, x) { return acc + x; }, [10, 20, 30, 40]);
+                     */
+                }
+            },
+            {
+                title: 'foldr',
+                body: function() {
+                    /*
+R.foldr(function(acc, x) { return acc + x; }, ['s', 'd', 'r', 'a', 'w', 'k', 'c', 'a', 'b']);
+                     */
+                }
+            }
+        ],
+        functions: [
+            {
+                title: "curry",
+                body: function () {
+                    /*
+var f = R.curry(function(a, b, c) { return b + (a * c); });
+f(2)(3)(4);
+                    */
+                }
+            },
+            {
+                title: "compose",
+                body: function () {
+                    /*
+var composeEx = R.compose(
+            R.map(function(x) { return x * x; }),
+            R.filter(function(x) { return x > 4; })
+            );
+composeEx([2, 3, 4, 5]);
+                     */
+                }
+            },
+            {
+                title: "pipe",
+                body: function () {
+                    /*
+var pipeEx = R.pipe(
+            R.map(function(x) { return x * x; }),
+            R.filter(function(x) { return x > 4; })
+            );
+pipeEx([2, 3, 4, 5]);
+                     */
+                }
+            },
+            {
+                title: 'useWith',
+                body: function() {
+                    /*
+var sum = function() { 
+    return R.foldl(function(acc, x) { return acc + x; }, 0, arguments);
+};
+var useWithEx = R.useWith(
+    sum,
+    function(y) { return y * 2; },
+    function(x) { return x * x; }
+);
+useWithEx(2, 3, 4);
+                     */
+                }
+            },
+            {
+                title: 'converge',
+                body: function() {
+                    /*
+var add = function(a, b) { return a + b; };
+var multiply = function(a, b) { return a * b; };
+var subtract = function(a, b) { return a - b; };
+var convergeEx = R.converge(multiply, add, subtract)
+convergeEx(2, 4);
+                     */
+                }
+            }
+        ],
+        objects: [
+            {
+                title: "prop",
+                body: function () {
+                    /*
+var propEx = R.prop('x');
+propEx({w: 'WWW', x: 'XXX', z: 'ZZZ'});
+                    */
+                }
+            }
+        ]
+    };
+
+    var lists = Object.keys(config);
+    //build lists
+    lists.forEach(function (name) {
+        elms[name] = document.getElementById( name );
+    });
+
+    lists.forEach( function (namespace) {
+        config[namespace].forEach(function (e, i) {
+            var str = e.body
+                .toString()
+                .replace(/[^\*]+\*/m, "")
+                .split("*/")[0]
+                .split("\n")
+                .filter(function (s) {
+                    return !!s && !s.match(/^\s+$/);
+                })
+                .join("\n");
+
+            cache[e.title] = str;
+            $('<li><a href="#">' + e.title + '</a></li>' ).appendTo(elms[namespace]);
+        });
+    } );
+
+    //add handlers
+    $("button[type=reset]" ).on("click", function () {
+        window.location.href = window.location.href.split("#")[0];
+    });
+
+    $(examples).on("click", function (e) {
+        cm.setValue( cache[e.target.textContent] );
+        e.preventDefault();
+    });
+
+}());
