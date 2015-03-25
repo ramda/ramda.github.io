3a2d7663159817671b90ed9564f81cee87c07f91
Michael Hurley <m_hur@yahoo.com>
2015-03-25 09:23:05 -0400

    fix fix for bitly oops oops

--- repl/assets/js/bitly.js
+++ repl/assets/js/bitly.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     $.getJSON("https://api-ssl.bitly.com/v3/shorten?", { 
         format: "json",
         access_token: "b3939439e77a7e13c8b0799c0eec70a276deee99",
-        longUrl: "http://ramdajs.com/repl" + encodeURIComponent(location.search + location.hash)
+        longUrl: "http://ramdajs.com/repl/" + location.search + location.hash
       },
       function(response) {
         $out.val(response.data.url);
