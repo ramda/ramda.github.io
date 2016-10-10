e978a32a21d8f3ce2263e483eedccdd52b03d7af
Matt McKellar-Spence <matt+git@mattms.com>
2016-10-10 17:30:32 +1100

    Convert publish.js to Ramda functions

--- publish.js
+++ publish.js
@@ -1,6 +1,10 @@
 var fs = require('fs');
 var path = require('path');
 
+var R = require('ramda');
+
+var {defaultTo, map, pipe, prop} = R
+
 var handlebars = require('handlebars');
 var hljs = require('highlight.js');
 var helper = require('jsdoc/util/templateHelper');
@@ -8,86 +12,98 @@ var marked = require('marked');
 
 var version = require('./package.json').devDependencies.ramda
 
-var headOr = function(x, xs) {
-  return xs.length === 0 ? x : xs[0];
-};
 
-var chain = function(f, xs) {
-  var result = [];
-  for (var idx = 0; idx < xs.length; idx += 1) {
-    result = result.concat(f(xs[idx]));
-  }
-  return result;
-};
+var prettifyCode = R.pipe(
+  R.join('\n'),
+  R.replace(/^[ ]{5}/gm, ''),
+  R.flip(R.invoker(2, 'highlight')('javascript'))(hljs),
+  R.prop('value')
+)
 
-var valuesForTitle = function(title, xs) {
-  var result = [];
-  for (var idx = 0; idx < xs.length; idx += 1) {
-    if (xs[idx].title === title) {
-      result.push(xs[idx].value);
-    }
-  }
-  return result;
-};
-
-var prettifySig = function(s) {
-  return s.replace(/[.][.][.]/g, '\u2026').replace(/->/g, '\u2192');
-};
-
-var prettifyCode = function(s) {
-  return hljs.highlight('javascript', s.join('\n').replace(/^[ ]{5}/gm, '')).value;
-};
+var prettifySig = R.pipe(
+  R.replace(/[.][.][.]/g, '\u2026'),
+  R.replace(/->/g, '\u2192')
+)
 
 //  simplifySee :: [String] -> [String]
 //
 //  Handles any combination of comma-separated and multi-line @see annotations.
-var simplifySee = function(xs) {
-  var result = [];
-  xs.forEach(function(x) {
-    x.split(/\s*,\s*/).forEach(function(s) {
-      result.push(s.replace(/^R[.]/, ''));
-    });
-  });
-  return result;
-};
+var simplifySee = R.pipe(
+  R.map(
+    R.pipe(
+      R.split(/\s*,\s*/),
+      R.map(R.replace(/^R[.]/, ''))
+    )
+  ),
+  R.flatten
+)
 
-var simplifyData = function(d) {
-  return {
-    aka: chain(function(s) { return s.split(/,\s*/); }, valuesForTitle('aka', d.tags)),
-    category: headOr('', valuesForTitle('category', d.tags)),
-    deprecated: d.deprecated == null ? '' : d.deprecated,
-    description: d.description == null ? '' : marked(d.description),
-    example: d.examples == null ? [] : prettifyCode(d.examples),
-    name: d.name == null ? '' : d.name,
-    params: d.params == null ? [] : d.params.map(function(p) {
-      return {
-        type: p.type.names[0] || '',
-        description: marked(p.description || ''),
-        name: p.name || ''
-      };
-    }),
+var titleFilter = pipe(R.propEq('title'), R.filter)
+
+var valueProp = R.chain(prop('value'))
+
+var simplifyData = R.applySpec({
+    aka: pipe(
+      prop('tags'),
+      titleFilter('aka'),
+      valueProp,
+      R.chain(R.split(/,\s*/))
+    ),
+    category: pipe(
+      prop('tags'),
+      titleFilter('category'),
+      valueProp,
+      R.head,
+      defaultTo('')
+    ),
+    deprecated: pipe(prop('deprecated'), defaultTo('')),
+    description: pipe(
+      prop('description'),
+      R.defaultTo(''),
+      marked
+    ),
+    example: pipe(
+      prop('examples'),
+      R.defaultTo(''),
+      prettifyCode
+    ),
+    name: pipe(prop('name'), defaultTo('')),
+    params: pipe(
+      prop('params'),
+      defaultTo([]),
+      map(R.applySpec({
+        description: pipe(
+          prop('description'),
+          defaultTo(''),
+          marked
+        ),
+        name: pipe(prop('name'), defaultTo('')),
+        type: pipe(R.path(['type', 'names', 0]), defaultTo(''))
+      }))
+    ),
     returns: {
-      type:
-        d.returns != null &&
-        d.returns[0] != null &&
-        d.returns[0].type != null &&
-        d.returns[0].type.names != null &&
-        d.returns[0].type.names[0] != null ?
-          d.returns[0].type.names[0] :
-          '',
-      description:
-        d.returns != null &&
-        d.returns[0] != null &&
-        d.returns[0].description != null ?
-          marked(d.returns[0].description) :
-          '',
+      description: pipe(R.path(['returns', 0, 'description']), defaultTo('')),
+      type: pipe(R.path(['returns', 0, 'type', 'names', 0]), defaultTo(''))
     },
-    see: d.see == null ? [] : simplifySee(d.see),
-    sigs: valuesForTitle('sig', d.tags).map(prettifySig),
-    since: d.since == null ? '' : d.since,
-    typedefns: valuesForTitle('typedefn', d.tags).map(prettifySig),
-  };
-};
+    see: pipe(
+      prop('see'),
+      defaultTo(''),
+      simplifySee
+    ),
+    sigs: pipe(
+      prop('tags'),
+      titleFilter('sig'),
+      valueProp,
+      map(prettifySig)
+    ),
+    since: pipe(prop('since'), defaultTo('')),
+    typedefns: pipe(
+      prop('tags'),
+      titleFilter('typedefn'),
+      valueProp,
+      map(prettifySig)
+    )
+})
 
 exports.publish = function(data, opts) {
   var templateFile = path.resolve(opts.destination, 'index.html.handlebars')
