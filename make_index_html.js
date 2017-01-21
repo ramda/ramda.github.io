360de697479219152175f1bc1ea8f1cb318c8c63
Matt McKellar-Spence <matt+git@mattms.com>
2017-01-21 21:12:05 +1100

    Use Pug templates for index and releases pages

--- make_index_html.js
+++ make_index_html.js
@@ -2,21 +2,22 @@ var fs = require('fs')
 
 var get_ramda_file = require('./get_ramda_file')
 
-var handlebars = require('handlebars')
-
 var marked = require('marked')
 
+var pug = require('pug')
+
 var version = require('./package.json').devDependencies.ramda
+
 
 get_ramda_file('README.md')
 .catch((err) => console.error(err))
 .then((readme_md) => {
   var readme_html = marked(readme_md)
 
-  var template = fs.readFileSync('index.html.handlebars', {encoding: 'utf8'})
+  var make_html = pug.compileFile('index.pug')
 
-  var html = handlebars.compile(template)({
-    readme: new handlebars.SafeString(readme_html),
+  var html = make_html({
+    readme: readme_html,
     version: version
   })
 
